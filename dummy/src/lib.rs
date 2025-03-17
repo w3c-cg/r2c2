028cab7e773348e659fe605fd2e68a90fea7a880
Pierre-Antoine Champin <pierre-antoine@w3.org>
2025-03-17 17:05:38 +0100

    make the repository a Cargo workspace
    the dummy crate will be removed as soon as we have one real crate.

---
+++ dummy/src/lib.rs
@@ -0,0 +1,18 @@
+//! Dummy crate, just used to make the whole repo a valid Cargo workspace.
+//!
+//! It will disappear as soon as the first real work-item is added.
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
