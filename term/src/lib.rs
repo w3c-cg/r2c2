0b4885a3a407e75ace687b77c08f1c2dd487e5e3
Pierre-Antoine Champin <pierre-antoine@w3.org>
2025-03-19 10:08:24 +0100

    Update term/src/lib.rs
    Add comment to clarify that the proposed design can be challenged.

--- term/src/lib.rs
+++ term/src/lib.rs
@@ -1,8 +1,10 @@
 //! TODO
+//! One POSSIBLE way forward is to
 //! 1. define or import simple wrapper types for building blocks
 //!    (IRIs, language tags...)
 //! 2. define traits for different kinds of terms
 //!    (Subject, Predicate, Object, GraphName)
+//! but the final design may be different.
 
 #[cfg(test)]
 mod test {}
