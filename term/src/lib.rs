a343dbc6bdfa8343aaf46643820acef962b7897a
Pierre-Antoine Champin <pierre-antoine@w3.org>
2025-03-19 09:26:43 +0100

    layout for a new work-item 'term'

---
+++ term/src/lib.rs
@@ -0,0 +1,8 @@
+//! TODO
+//! 1. define or import simple wrapper types for building blocks
+//!    (IRIs, language tags...)
+//! 2. define traits for different kinds of terms
+//!    (Subject, Predicate, Object, GraphName)
+
+#[cfg(test)]
+mod test {}
